bba37fd66e3b490718a401a0694d0cf403a8389d
AleME2002 <martinezevansalejo@gmail.com>
2026-08-29 19:29:21 -0300

    segui ejercicio 3

--- Taller1/Ejercicio3.cpp
+++ Taller1/Ejercicio3.cpp
@@ -3,10 +3,40 @@
 
 using namespace std;
 
-vector <vector <int >> arbol;
-vector <int > lista;
-vector <int > color; // -1 = sin visitar , 0 y 1 = los dos colores
+vector <vector <char >> letras;
+vector <string > nombres;
 
-// Sin identificar = -1
-// Viendo Actualmente = 0
-// Ya visto = 1
+string lexicoNoSeQue(){
+    
+    int longitudPalabraActual = 0;
+    for (int i = 0; i < nombres.size() - 1; i++){
+        longitudPalabraActual = nombres[i].size();
+        if (nombres[i] == nombres[i + 1])
+            break;
+        for (int j = 0; j < nombres[longitudPalabraActual].size(); j++){
+            string nombre1 = nombres[i];
+            string nombre2 = nombres[i + 1];
+            if (nombre1[j] == nombre2[j]){
+                break;
+            }
+            else if (nombre1[j] != nombre2[j]){
+                
+            }
+        }
+    }
+
+
+    return "";
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    nombres.resize(n);
+    letras.resize(26);
+    for (int i = 0; i <= n; i++) {
+        cin >> nombres[i];
+    }
+    cout << lexicoNoSeQue() << "\n";
+}
