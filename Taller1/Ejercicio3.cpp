563bd813c3035da5e221e607e3487f0217e3cd96
AleME2002 <martinezevansalejo@gmail.com>
2026-08-29 22:41:28 -0300

    Refactor lexicoNoSeQue function to improve logic and readability

--- Taller1/Ejercicio3.cpp
+++ Taller1/Ejercicio3.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector <vector <char >> letras;
 vector <string > nombres;
 
+// a=0, b=1, ..., z=25
+
+//  g    p
+// [p], [ ]
+
+/*
+Si una letra esta antes q otra, pongo a la letra1 en la lista de la letra2
+Ejemplo
+nombres = ["pedro", "alejo"]         
+'p' esta antes q 'a' por lo tanto queda
+        a   b        p       z
+letras[[ ],[ ],...,['a'],...[ ]]
+
+*/
+
 string lexicoNoSeQue(){
     
     int longitudPalabraActual = 0;
-    for (int i = 0; i < nombres.size() - 1; i++){
-        longitudPalabraActual = nombres[i].size();
-        if (nombres[i] == nombres[i + 1])
+    for (int i = 0; i < nombres.size() - 1; i++){                                   // Itero sobre los nombres
+        longitudPalabraActual = min(nombres[i].size(), nombres[i+1].size());        // Agarro la palabra mas chica ya que si nombre[i+1] fuera mas chica, habria problemas
+        if (nombres[i] == nombres[i + 1])                                           // Si son el mismo nombre la corta
             break;
-        for (int j = 0; j < nombres[longitudPalabraActual].size(); j++){
-            string nombre1 = nombres[i];
-            string nombre2 = nombres[i + 1];
-            if (nombre1[j] == nombre2[j]){
-                break;
-            }
-            else if (nombre1[j] != nombre2[j]){
-                
+        for (int j = 0; j < longitudPalabraActual; j++){                            // Itero entre las letras de las palabras
+            char letra1 = nombres[i][j];
+            char letra2 = nombres[i + 1][j];
+            if (letra1 != letra2){                                                  // Si son distintas, meto a la letra1 en la lista de la letra2
+                letras[letra2 - 'a'].push_back(letra1);
+                break;                                                              // Lo corto para q no siga comparando letras
             }
         }
     }
 
-
+    
     return "";
 }
 
